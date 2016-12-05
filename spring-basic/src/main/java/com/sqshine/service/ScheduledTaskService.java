73a4b8d16add89a94ae6a7fba2912fc71e943760
shiquan <shiquan@flybycloud.com>
2016-12-05 10:26:49 +0800

    使用java 8 时间表示方式

--- spring-basic/src/main/java/com/sqshine/service/ScheduledTaskService.java
+++ spring-basic/src/main/java/com/sqshine/service/ScheduledTaskService.java
@@ -6,18 +6,21 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class ScheduledTaskService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss n");
+
 
     @Scheduled(fixedDelay = 5000)
     public void outputCurrentTime() {
-        logger.info("每隔5秒执行一次:{}", LocalDateTime.now());
+        logger.info("每隔5秒执行一次:{}", LocalDateTime.now().format(dateTimeFormatter));
     }
 
-    @Scheduled(cron = "0 18 10 ? * *")
+    @Scheduled(cron = "0 26 10 ? * *")
     public void fixTimeExecution() {
         logger.info("在指定的时间执行:{}", LocalDateTime.now());
     }
