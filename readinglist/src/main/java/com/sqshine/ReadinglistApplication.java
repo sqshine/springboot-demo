17db57bf6b7e725d04fadc9e65ca5b9d95059e40
shiquan <shiquan@flybycloud.com>
2017-01-05 10:21:41 +0800

    修改为jar包部署方式

--- readinglist/src/main/java/com/sqshine/ReadinglistApplication.java
+++ readinglist/src/main/java/com/sqshine/ReadinglistApplication.java
@@ -6,12 +6,11 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication
-public class ReadinglistApplication extends SpringBootServletInitializer{
+public class ReadinglistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReadinglistApplication.class, args);
