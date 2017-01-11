ad55373919d62b6f5c65e5c80fc7519d8db8207c
shiquan <shiquan@flybycloud.com>
2017-01-11 16:24:42 +0800

    事务的处理修改

--- readinglist/src/main/java/com/sqshine/readinglist/service/CountryService.java
+++ readinglist/src/main/java/com/sqshine/readinglist/service/CountryService.java
@@ -6,8 +6,8 @@ import com.sqshine.readinglist.domain.model.Country;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
